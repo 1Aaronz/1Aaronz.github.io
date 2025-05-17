d6a9195809c4c49b5d794487f0ce8c2ff0b302a6
1Aaronz <arz@geneseo.edu>
2025-05-17 01:43:48 -0400

    update

--- notes.R
+++ notes.R
@@ -1,1 +1,28 @@
-R markdown
+# R markdown
+# Heading 1
+## Heading 2
+### Heading 3
+
+*italic* or _italic_
+**bold** or __bold__
+
+- Item 1
+- Item 2
+- Subitem 2.1
+- Subitem 2.2
+
+1. First item
+2. Second item
+
+[DANL 210](https://bcdanl.github.io/210)
+![Geneseo Logo](https://bcdanl.github.io/img/geneseo-logo.gif)
+
+
+```
+"Code block"
+```
+
+```python
+# Python code block
+import numpy as np
+```
